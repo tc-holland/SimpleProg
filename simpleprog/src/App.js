9f01918a5cf4fb402dc9c9ea82105ce78fbf14a5
Simi Ajani <sajani62705@gmail.com>
2025-10-14 14:52:21 -0400

    Edited App.js and App.css with login page stuff

--- simpleprog/src/App.js
+++ simpleprog/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 //including some changes bc github wants to be stupid
@@ -19,6 +19,47 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App; */
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    // MAIN WRAPPER DIV FOR ENTIRE PAGE
+    <div className="login-container">
+      {/* PAGE TITLE */}
+      <h1>Student Login</h1>
+
+      {/* LOGIN FORM STARTS HERE */}
+      <form className="login-form">
+        <label htmlFor="email">Email:</label> {/* EMAIL LABEL AND INPUT */}
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          required
+        />
+
+        <label htmlFor="password">Password:</label> {/* PASSWORD LABEL AND INPUT */}
+        <input
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          required
+        />
+
+        <button type="submit">Log In</button> {/* SUBMIT BUTTON */}
+      </form>
+
+      {/* FORGOT PASSWORD LINK */}
+      <p className="forgot-password">
+        <a href="#">Forgot your password?</a>
+      </p>
     </div>
   );
 }
