054019fd10082e4f7025621471baed38e3a0aa8e
Hannah Imler <hannah.imler@ufl.edu>
2025-10-16 10:24:20 -0400

    Landing Page

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login"
 import Signup from "./components/Signup";
+import Landing from "./components/Landing";
 
 function App() {
 
@@ -10,8 +11,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />}/>
         <Route path="/signup" element={<Signup />}/>
+        <Route path="/landing" element={<Landing />}/>
         {/*Default route login*/}
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/landing" />} />
       </Routes>
     </Router>
   );
